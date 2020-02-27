dac1deb98030d5c09fdfa409aea76b28db7a5889
fujinran <853804270@qq.com>
2020-02-27 21:09:28 +0800

    INIT

---
+++ run.sh
@@ -0,0 +1,21 @@
+SERVICE_NAME=${PWD##*/}
+JAR=$SERVICE_NAME.jar
+MPORT=8050
+echo ">>> git pull"
+git pull https://github.com/MSDH233/testhzero-swagger.git
+
+echo ">>> mvn clean package -Dmaven.javadoc.skip=true"
+mvn clean package -Dmaven.javadoc.skip=true
+
+echo ">>> cd target"
+cd target
+mv app.jar $JAR
+
+echo ">>> kill -9 $(lsof -t -sTCP:LISTEN -i:$MPORT)"
+kill -9 $(lsof -t -sTCP:LISTEN -i:$MPORT)
+
+JAVA_OPTS="-Xms300M -Xmx300M -Dspring.profiles.active=dev"
+echo ">>> nohup java -jar $JAVA_OPTS $JAR >app.log &"
+BUILD_ID=dontKillMe nohup java -jar $JAVA_OPTS $JAR >app.log &
+
+
